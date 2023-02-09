9a84da65300e0d5221c4ce2cf8b53dcd0c695591
Bukhari_Inst <bukhari414647@gmail.com>
2023-02-09 09:04:44 +0700

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
@@ -29,8 +27,5 @@ class TaskController extends Controller
             ];
         }
         return response()->json($data);
-
-        // return TaskResource::collection($task->category->name);
-
     }
 }
